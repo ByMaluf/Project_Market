1a67d7378b57a5044b27bac803fd948be783e036
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-01-05 12:16:04 -0400

    Estilização de cores + Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,18 +5,20 @@ import { faCartShopping, faHeart, faUser } from '@fortawesome/free-solid-svg-ico
 
 export default function Header() {
   return (
-    <header className={style.cabecalho}>
-      <div className={style.containerImg}>
-        <img src="" alt="" />
-        <h1><Link to={'/'}>ByMarket</Link></h1>
+    <header className={style.containerHeader} >
+      <div className={style.cabecalho}>
+        <div className={style.containerImg}>
+          <img src="" alt="" />
+          <h1><Link to={'/'}>ByMarket</Link></h1>
+        </div>
+        <nav>
+          <ul>
+            <li className={style.itemNavigation}><FontAwesomeIcon icon={faUser} size='sm' /> <a href="#"> Minha Conta</a></li>
+            <li className={style.itemNavigation}><FontAwesomeIcon icon={faHeart} size='sm' /> <a href="#"> Meus Favoritos</a></li>
+            <li className={style.itemNavigation}><FontAwesomeIcon icon={faCartShopping} size='sm' /><a href="#">  Carrinho</a></li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li className={style.itemNavigation}><FontAwesomeIcon icon={faUser} size='sm' /> <a href="#"> Minha Conta</a></li>
-          <li className={style.itemNavigation}><FontAwesomeIcon icon={faHeart} size='sm' /> <a href="#"> Meus Favoritos</a></li>
-          <li className={style.itemNavigation}><FontAwesomeIcon icon={faCartShopping} size='sm' /><a href="#">  Carrinho</a></li>
-        </ul>
-      </nav>
     </header>
   );
 }
